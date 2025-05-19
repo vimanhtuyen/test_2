99899c174b8a20246a3f6bd47c2569ac0164ee55
vimanhtuyen <vimanhtuyen@gmail.com>
2025-05-19 18:19:59 +0700

    Initial commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\viman\OneDrive\Desktop\Web_Python\Lesson_5\django_travel_blog_full_project\env
+set VIRTUAL_ENV=C:\Users\viman\OneDrive\Desktop\Web_Python\Lesson_7\django_travel_blog_full_project\env
 
 if not defined PROMPT set PROMPT=$P$G
 
